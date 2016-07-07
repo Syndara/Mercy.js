ebdeaca4942f65cb41acb95628af16d99bd17ccf
Syndara <tachkan@gmail.com>
2016-07-07 15:30:30 -0400

    Update mercy.js

--- mercy.js
+++ mercy.js
@@ -286,6 +286,10 @@ bot.on('message', function (user, userID, channelID, message, rawEvent) {
         });
 	}
 	
+	if (message === userPre + "p") {
+		getPic("http://photobucket.com/recentuploads?filter=image&page=1");
+	}
+	
 	// Simple function just returns a date object at the current time.
 	function getTime() {
 		return new Date();
@@ -325,6 +329,25 @@ bot.on('message', function (user, userID, channelID, message, rawEvent) {
 			});
 			
 		}
+		})
+	}
+	
+	function getPic(theURL) {
+		var request = require('request');
+		request(theURL, function (error, response, body) {
+			if (!error && response.statusCode == 200) {
+			
+				imageStart = body.indexOf("secureFullsize");
+				imageUrl = body.substring((imageStart + 17), (imageStart + 150))
+				imageEnd = imageUrl.indexOf("\"");
+				imageUrl = imageUrl.substring(0,imageEnd)
+				imageUrl = imageUrl.replace(/\\\//g, "/");
+				
+				bot.sendMessage({
+					to: channelID,
+					message: imageUrl
+				});
+			}
 		})
 	}
 
