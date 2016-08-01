786cf210d4fa22e1256c6506d94a86d1eea73caf
Syndara <tachkan@gmail.com>
2016-08-01 16:40:59 -0400

    Update handler.js

--- handler.js
+++ handler.js
@@ -117,7 +117,8 @@ module.exports = {
 		var OSongs = [
 		"./Music/Other/O_Short.mp3",
 		"./Music/Other/O_Long.mp3",
-		"./Music/Other/O_Reverse.mp3"]
+		"./Music/Other/O_Reverse.mp3",
+		"./Music/Other/O.mp3"]
 		
 		var ch = functions.listVoiceChannels(bot, channelID, userID);
 		var song = OSongs[Math.floor(Math.random() * OSongs.length)];
