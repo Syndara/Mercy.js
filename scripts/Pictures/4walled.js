cf3122fd16ec1359d4c39f70aac27bf5bc8acd5c
Syndara <tachkan@gmail.com>
2016-10-09 13:52:17 -0400

    Update 4walled.js

--- scripts/Pictures/4walled.js
+++ scripts/Pictures/4walled.js
@@ -4,12 +4,12 @@ module.exports = {
 		request(theURL, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 			
-				imageStart = body.indexOf("//4walled.cc/show");
-				imageUrl = "https:" + body.substring((imageStart), (imageStart + 290))
-				resStart = imageUrl.indexOf("Resolution");
-				resText = imageUrl.substring((resStart), (resStart + 40))
-				resEnd = resText.indexOf("'")
-				resolution = resText.substring(0, resEnd)
+				//imageStart = body.indexOf("//4walled.cc/show");
+				//imageUrl = "https:" + body.substring((imageStart), (imageStart + 290))
+				//resStart = imageUrl.indexOf("Resolution");
+				//resText = imageUrl.substring((resStart), (resStart + 40))
+				//resEnd = resText.indexOf("'")
+				//resolution = resText.substring(0, resEnd)
 				//imageEnd = imageUrl.indexOf("target") - 2
 				//imageUrl = imageUrl.substring(0, imageEnd)
 				
@@ -22,16 +22,25 @@ module.exports = {
 					images.push(href)
 				});
 				
-				console.log(images);
+				var random = Math.floor(Math.random() * images.length);
+				while (random <= 4) {
+					random = Math.floor(Math.random() * images.length);
+				}
 				
-				var imageUrl = "http:" + images[Math.floor(Math.random() * images.length) + 6];
-				console.log(imageUrl);
+				var imageUrl = "http:" + images[random];
 				
 				
 					request(imageUrl, function (error, response, body) {
 						if (!error && response.statusCode == 200) {
 							imageStart = body.indexOf("//4walled.cc/src");
-							imageUrl = "https:" + body.substring((imageStart), (imageStart + 90))
+							imageUrl = "https:" + body.substring((imageStart), (imageStart + 200))
+							resStart = body.indexOf("<span id=\"info_resolution\">");
+							resStart = body.substring(resStart + 1, resStart + 50);
+							low = resStart.indexOf(">");
+							resStart = resStart.substring(low + 1, resStart.indexOf("<"))
+							resolution = "Resolution: " + resStart;
+							
+							console.log(resStart);
 							imageEnd = imageUrl.indexOf("\"")
 							imageUrl = imageUrl.substring(0, imageEnd)
 							
